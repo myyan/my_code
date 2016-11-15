79da621f9e1df0f530bc0b61395ef72a6a4e5974
chenhao <chenhao@maxtropy.com>
2016-11-15 11:46:35 +0800

    add the creative mind about design a seems unbelievable things

---
+++ src/com/freeland/util/design/GoldBridge.java
@@ -0,0 +1,31 @@
+package com.freeland.util.design;
+
+/**
+ * Created by chenhao on 2016/11/15.
+ */
+public class GoldBridge {
+
+    /**
+     * How to statistics one week entry flow of the golden bridge
+     *
+     * You only have the fifteen minutes to think about it
+     *
+     * As for me :
+     *
+     * 1.By the car or sensors
+     * evaluate the corresponding flow
+     *
+     * By the air conditional
+     *
+     * 2.yeah air specific item including
+     *
+     *
+     * 3.and the
+     *
+     * tourist journey holiday
+     *
+     * pronunciation pronounce
+     *
+     * freeland 
+     * */
+}
---
+++ src/com/freeland/util/sort/InsertSort.java
@@ -0,0 +1,8 @@
+package com.freeland.util.sort;
+
+/**
+ * Created by chenhao on 2016/11/15.
+ */
+public class InsertSort {
+
+}
